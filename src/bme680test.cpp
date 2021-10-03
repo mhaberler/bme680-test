0dfa5028b6c764b78ca53f088b3e56fc6e6a4463
Michael Haberler <git@mah.priv.at>
2021-10-04 00:15:41 +0200

    add lps22, tmp117 sensors

--- src/bme680test.cpp
+++ src/bme680test.cpp
@@ -19,68 +19,136 @@
 #include <SPI.h>
 #include <Adafruit_Sensor.h>
 #include "Adafruit_BME680.h"
+#include <Adafruit_TMP117.h>
+#include <Adafruit_LPS2X.h>
 
-#define BME_SCK 13
-#define BME_MISO 12
-
-#define BME_MOSI 11
-#define BME_CS 10
+#include "I2CScanner.h"
 
 #define SEALEVELPRESSURE_HPA (1013.25)
 
 Adafruit_BME680 bme; // I2C
-//Adafruit_BME680 bme(BME_CS); // hardware SPI
-//Adafruit_BME680 bme(BME_CS, BME_MOSI, BME_MISO,  BME_SCK
+
+#define LPS22_ADDR 0x5C
+Adafruit_LPS22 lps;
+Adafruit_TMP117 tmp117;
+
+I2CScanner scanner;
+
+bool have_tmp117, have_lps22, have_bme680;
 
 void setup()
 {
-  Serial.begin(115200);
-  while (!Serial)
+    Serial.begin(115200);
+    while (!Serial)
+        delay(10);
     ;
-  Serial.println(F("BME680 tester   xxxx"));
 
-  if (!bme.begin())
-  {
-    Serial.println("Could not find a valid BME680 sensor, check wiring!");
-    while (1)
-      ;
-  }
+    scanner.Init();
+    scanner.Scan();
+    Serial.println(F("sensor test"));
 
-  // Set up oversampling and filter initialization
-  bme.setTemperatureOversampling(BME680_OS_8X);
-  bme.setHumidityOversampling(BME680_OS_2X);
-  bme.setPressureOversampling(BME680_OS_4X);
-  bme.setIIRFilterSize(BME680_FILTER_SIZE_3);
-  bme.setGasHeater(320, 150); // 320*C for 150 ms
+    have_tmp117 = tmp117.begin();
+    have_bme680 = bme.begin();
+    have_lps22 = lps.begin_I2C(LPS22_ADDR);
+
+    Serial.printf("have_tmp117=%d  have_lps22=%d  have_bme680=%d\n", have_tmp117, have_lps22, have_bme680);
+    // Try to initialize!
+    if (have_lps22)
+
+    {
+        Serial.println("LPS22 Found!");
+        Serial.print("Data rate set to: ");
+        switch (lps.getDataRate())
+        {
+        case LPS25_RATE_ONE_SHOT:
+            Serial.println("One Shot");
+            break;
+        case LPS25_RATE_1_HZ:
+            Serial.println("1 Hz");
+            break;
+        case LPS25_RATE_7_HZ:
+            Serial.println("7 Hz");
+            break;
+        case LPS25_RATE_12_5_HZ:
+            Serial.println("12.5 Hz");
+            break;
+        case LPS25_RATE_25_HZ:
+            Serial.println("25 Hz");
+            break;
+        }
+    }
+    else
+    {
+
+        Serial.println("Failed to find LPS22 chip");
+    }
+    
+    if (have_bme680)
+    {
+
+        // Set up oversampling and filter initialization
+        bme.setTemperatureOversampling(BME680_OS_8X);
+        bme.setHumidityOversampling(BME680_OS_2X);
+        bme.setPressureOversampling(BME680_OS_4X);
+        bme.setIIRFilterSize(BME680_FILTER_SIZE_3);
+        bme.setGasHeater(320, 150); // 320*C for 150 ms;
+    }
 }
 
 void loop()
 {
-  if (!bme.performReading())
-  {
-    Serial.println("Failed to perform reading :(");
-    return;
-  }
-  Serial.print("Temperature = ");
-  Serial.print(bme.temperature);
-  Serial.println(" *C");
+    sensors_event_t temp; // create an empty event to be filled
 
-  Serial.print("Pressure = ");
-  Serial.print(bme.pressure / 100.0);
-  Serial.println(" hPa");
+    if (have_bme680)
+    {
 
-  Serial.print("Humidity = ");
-  Serial.print(bme.humidity);
-  Serial.println(" %");
+        if (!bme.performReading())
+        {
+            Serial.println("Failed to perform reading :(");
+            return;
+        }
+        Serial.print("bme680 Temperature = ");
+        Serial.print(bme.temperature);
+        Serial.println(" *C");
 
-  Serial.print("Gas = ");
-  Serial.print(bme.gas_resistance / 1000.0);
-  Serial.println(" KOhms");
+        Serial.print("bme680 Pressure = ");
+        Serial.print(bme.pressure / 100.0);
+        Serial.println(" hPa");
 
-  Serial.print("Approx. Altitude = ");
-  Serial.print(bme.readAltitude(SEALEVELPRESSURE_HPA));
-  Serial.println(" m");
+        Serial.print("bme680 Humidity = ");
+        Serial.print(bme.humidity);
+        Serial.println(" %");
 
-  Serial.println();
-  delay(2000);
+        Serial.print("bme680 Gas = ");
+        Serial.print(bme.gas_resistance / 1000.0);
+        Serial.println(" KOhms");
+
+        Serial.print("bme680 Approx. Altitude = ");
+        Serial.print(bme.readAltitude(SEALEVELPRESSURE_HPA));
+        Serial.println(" m");
+
+        Serial.println();
+    }
+
+    if (have_tmp117)
+    {
+        tmp117.getEvent(&temp); //fill the empty event object with the current measurements
+        Serial.print("TMP117 Temperature  ");
+        Serial.print(temp.temperature);
+        Serial.println(" degrees C");
+        Serial.println("");
+    }
+    if (have_lps22)
+    {
+        sensors_event_t pressure;
+        lps.getEvent(&pressure, &temp); // get pressure
+        Serial.print("LPS22 Temperature: ");
+        Serial.print(temp.temperature);
+        Serial.println(" degrees C");
+        Serial.print("LPS22 Pressure: ");
+        Serial.print(pressure.pressure);
+        Serial.println(" hPa");
+        Serial.println("");
+    }
+    delay(2000);
 }
